f93128aba4e4dd035b067c7e61ba298a17b0a2a8
horko152 <voloshenyuk98@gmail.com>
2020-08-09 18:15:51 +0300

    Release

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './pages/App';
+
+import './assets/reset.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
