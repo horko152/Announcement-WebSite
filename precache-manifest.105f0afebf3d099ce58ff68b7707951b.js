de6352f97ddba82975fdd19780a963eb474948d5
horko152 <voloshenyuk98@gmail.com>
2020-08-10 12:29:59 +0300

    Updates

---
+++ precache-manifest.105f0afebf3d099ce58ff68b7707951b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305ab2365d462a6ad3eae5630433a059",
+    "url": "/Announcement-WebSite/index.html"
+  },
+  {
+    "revision": "053cfa5f72804476a1d8",
+    "url": "/Announcement-WebSite/static/css/main.20021f81.chunk.css"
+  },
+  {
+    "revision": "fc0598c70d12098ee75f",
+    "url": "/Announcement-WebSite/static/js/2.89697435.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Announcement-WebSite/static/js/2.89697435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053cfa5f72804476a1d8",
+    "url": "/Announcement-WebSite/static/js/main.8e926329.chunk.js"
+  },
+  {
+    "revision": "502feb246296fce4a669",
+    "url": "/Announcement-WebSite/static/js/runtime-main.3ca4130f.js"
+  }
+]);
